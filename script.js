b8d818b8663d9c096bd49c409d01eac8164567ae
coboyle444 <courtlyn.oboyle@edu.sait.ca>
2025-03-23 12:52:44 -0600

    duplicating joke formula for categories

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("title").textContent = "Joke of the Day";
+document.getElementById("title").textContent = "😹 Joke of the Day ";
 
 fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
 .then(response => response.json ())
@@ -12,4 +12,32 @@ fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
     }
 });
 
+programming.addEventListener('click', function () {
+    console.log("program joke!");
+    fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode')
+    .then(response => response.json ())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke
+        }
+        else {
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+    })
+});
 
+misc.addEventListener('click', function () {
+    console.log("miscellaneous joke!");
+    fetch('https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode')
+    .then(response => response.json ())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke
+        }
+        else {
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+    })
+});
