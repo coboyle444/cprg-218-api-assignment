c9a7e07958741018aca96304916e2e5935edca76
coboyle444 <courtlyn.oboyle@edu.sait.ca>
2025-03-23 14:17:08 -0600

    title change after category change YAY!

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById("title").textContent = "😹 Joke of the Day ";
+const categoryTitle = document.getElementById(('title'))
+const joke = document.getElementById("joke");
+
+categoryTitle.textContent = "😹 Joke of the Day";
 
 fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
 .then(response => response.json ())
@@ -14,6 +17,7 @@ fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
 
 programming.addEventListener('click', function () {
     console.log("program joke!");
+    categoryTitle.textContent = "A Random Programming Joke 💻"
     fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode')
     .then(response => response.json ())
     .then(data => {
@@ -29,7 +33,56 @@ programming.addEventListener('click', function () {
 
 misc.addEventListener('click', function () {
     console.log("miscellaneous joke!");
+    categoryTitle.textContent = "A Random Miscellaneous Joke 😄"
     fetch('https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode')
+    .then(response => response.json ())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke
+        }
+        else {
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+    })
+});
+
+pun.addEventListener('click', function () {
+    console.log("pun... *sigh*");
+    categoryTitle.textContent = "A Random Pun 🙃";
+    fetch('https://v2.jokeapi.dev/joke/Pun?safe-mode')
+    .then(response => response.json ())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke
+        }
+        else {
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+    })
+});
+
+christmas.addEventListener('click', function () {
+    console.log("christmas joke!");
+    categoryTitle.textContent = "A Random Christmas Joke 🎄";
+    fetch('https://v2.jokeapi.dev/joke/Christmas?safe-mode')
+    .then(response => response.json ())
+    .then(data => {
+        console.log(data);
+        if(data.type === "single"){
+            joke.textContent = data.joke
+        }
+        else {
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+    })
+});
+
+spooky.addEventListener('click', function () {
+    console.log("spooky joke! oooOOOoo!");
+    categoryTitle.textContent = "A Random Spooky Joke 👻";
+    fetch('https://v2.jokeapi.dev/joke/Spooky?safe-mode')
     .then(response => response.json ())
     .then(data => {
         console.log(data);
