ecedbbb7562d68719dcc18ef8bd712526daf1d63
coboyle444 <courtlyn.oboyle@edu.sait.ca>
2025-03-23 12:13:18 -0600

    java starter - will this work?

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+.then(response => response.json ())
+.then(data => {
+    console.log(data);
+    if(data.type === "single") {
+        joke.textContent = data.joke
+    }
+    else if (data.type === "twopart") {
+        joke.textContent = data.setup + "..." + data.delivery;
+    }
+});
