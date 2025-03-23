f378855aae05a4bedf7bb8a56a8e32304d1eca11
coboyle444 <courtlyn.oboyle@edu.sait.ca>
2025-03-23 14:26:40 -0600

    style: bg colors for joke categories

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const categoryTitle = document.getElementById(('title'))
 const joke = document.getElementById("joke");
+joke.style.backgroundColor = "#cccc";
+joke.style.borderColor = "#000000";
+joke.style.borderStyle = "solid";
 
 categoryTitle.textContent = "😹 Joke of the Day";
 
@@ -18,6 +21,8 @@ fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
 programming.addEventListener('click', function () {
     console.log("program joke!");
     categoryTitle.textContent = "A Random Programming Joke 💻"
+    joke.style.backgroundColor = "#7393B3";
+    joke.style.borderColor = "#0047AB";
     fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode')
     .then(response => response.json ())
     .then(data => {
@@ -34,6 +39,8 @@ programming.addEventListener('click', function () {
 misc.addEventListener('click', function () {
     console.log("miscellaneous joke!");
     categoryTitle.textContent = "A Random Miscellaneous Joke 😄"
+    joke.style.backgroundColor = "#FFFF8F";
+    joke.style.borderColor = "#FFBF00";
     fetch('https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode')
     .then(response => response.json ())
     .then(data => {
@@ -50,6 +57,8 @@ misc.addEventListener('click', function () {
 pun.addEventListener('click', function () {
     console.log("pun... *sigh*");
     categoryTitle.textContent = "A Random Pun 🙃";
+    joke.style.backgroundColor = "#FFAA33";
+    joke.style.borderColor = "#FFFF00";
     fetch('https://v2.jokeapi.dev/joke/Pun?safe-mode')
     .then(response => response.json ())
     .then(data => {
@@ -66,6 +75,8 @@ pun.addEventListener('click', function () {
 christmas.addEventListener('click', function () {
     console.log("christmas joke!");
     categoryTitle.textContent = "A Random Christmas Joke 🎄";
+    joke.style.backgroundColor = "#AFE1AF";
+    joke.style.borderColor = "#D70040";
     fetch('https://v2.jokeapi.dev/joke/Christmas?safe-mode')
     .then(response => response.json ())
     .then(data => {
@@ -82,6 +93,7 @@ christmas.addEventListener('click', function () {
 spooky.addEventListener('click', function () {
     console.log("spooky joke! oooOOOoo!");
     categoryTitle.textContent = "A Random Spooky Joke 👻";
+    joke.style.backgroundColor = "#FAC898";
     fetch('https://v2.jokeapi.dev/joke/Spooky?safe-mode')
     .then(response => response.json ())
     .then(data => {
